70a8635c096a319b1b42ce5d8fc0e09ad867bb5e
hanjoo0211 <hanjoo0211@gmail.com>
2023-02-23 15:20:19 +0900

    feat: set urls and views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+import openai
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+model_engine = "text-davinci-002"
+
+@csrf_exempt
+def chatbot(request):
+    if request.method == "POST":
+        user_input = request.POST.get("input", "")
+        prompt = f"User: {user_input}\nBot:"
+        response = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=50)
+        bot_response = response.choices[0].text.strip()
+        return JsonResponse({"response": bot_response})
+    else:
+        return JsonResponse({"error": "Invalid request method."})
